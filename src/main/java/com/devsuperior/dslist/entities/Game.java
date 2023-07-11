bc9b789f0eb72cc2b0c77bb1f7073e86900ceb55
Lucas Phelipe Castanho Ribeiro <pheliperibeiro2003@gmail.com>
2023-07-11 00:45:23 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dslist.entities;
 
-
-
-import jakarta.persistence.*;
-
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
@@ -15,24 +18,31 @@ public class Game {
     private Long id;
     private String title;
 
-    @Column (name = "game_year")
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private String imgURL;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game (Long id, String title, Integer year, String genre, String platform, String imgURL, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+                String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
-        this.imgURL = imgURL;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -40,48 +50,71 @@ public class Game {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public Integer getYear() {
         return year;
     }
+
     public void setYear(Integer year) {
         this.year = year;
     }
+
     public String getGenre() {
         return genre;
     }
+
     public void setGenre(String genre) {
         this.genre = genre;
     }
-    public String getPlatform() {
-        return platform;
+
+    public String getPlatforms() {
+        return platforms;
     }
-    public void setPlatform(String platform) {
-        this.platform = platform;
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
-    public String getImgURL() {
-        return imgURL;
+
+    public Double getScore() {
+        return score;
     }
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
+
+    public void setScore(Double score) {
+        this.score = score;
     }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
     public String getShortDescription() {
         return shortDescription;
     }
+
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
+
     public String getLongDescription() {
         return longDescription;
     }
+
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
